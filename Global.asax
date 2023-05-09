537b8831b39fc8e3fe8c22a024e369487943333f
anandmand <mandadi.anand@gmail.com>
2023-05-09 09:30:42 +0530

    Add files via upload
    Azure POCS code done on 9 May

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureAppserviceTesting.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureAppserviceTesting.Startup))]
+namespace AzureAppserviceTesting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
